d6d1b9c30b53edcfbbb70a9b882eca04b76fa73e
Haany Ali <asterp04@gmail.com>
2023-06-26 13:02:03 -0700

    cache images;smooth anim

--- lib/models/announcement_data.dart
+++ lib/models/announcement_data.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+enum AnnouncementType { EVENT, ANNOUNCEMENT }
+
 class AnnouncementData {
   late final bool actionBtnDisplay;
   late final String actionBtnRedirect;
@@ -9,6 +11,7 @@ class AnnouncementData {
   late final List<String> images;
   late final String name;
   late final Timestamp timestamp;
+  late final String type;
 
   AnnouncementData(
       {required this.actionBtnDisplay,
@@ -18,10 +21,12 @@ class AnnouncementData {
       required this.description,
       required this.images,
       required this.name,
+      required this.type,
       required this.timestamp});
 
   factory AnnouncementData.fromDocumentSnapshot(DocumentSnapshot response) {
     final snapshot = response.data() as Map<String, dynamic>;
+
     return AnnouncementData(
       actionBtnText: snapshot['action_btn_text'] ?? "",
       actionBtnRedirect: snapshot['action_btn_redirect'] ?? "",
@@ -32,6 +37,7 @@ class AnnouncementData {
               .map((e) => e.toString())
               .toList() ??
           [],
+      type: snapshot['type'] ?? "",
       name: snapshot['name'] ?? "",
       timestamp: snapshot['timestamp'],
     );
--- lib/widgets/announcement.dart
+++ lib/widgets/announcement.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cybersec_news/hackernews_api/helper/constants.dart';
 import 'package:cybersec_news/models/announcement_data.dart';
@@ -53,84 +54,186 @@ class AnnouncementTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 3 / 4,
-      child: Container(
-        alignment: Alignment.center,
-        decoration: BoxDecoration(
-            color: Color(0xFF24366E),
-            image: DecorationImage(
-                opacity: 0.4,
-                image: data.backdrop.isEmpty
-                    ? const AssetImage('assets/nyit_bear.png')
-                    : NetworkImage(data.backdrop) as ImageProvider)),
-        margin: EdgeInsets.symmetric(vertical: 10),
-        width: MediaQuery.of(context).size.width * 0.85,
-        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 5),
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Flexible(
-              child: Text(
-                data.name,
-                textAlign: TextAlign.left,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: Theme.of(context).textTheme.displaySmall!.fontSize,
-                  fontWeight: FontWeight.w800,
+    return InkWell(
+      onTap: () {
+        showModalBottomSheet(
+            context: context,
+            isScrollControlled: true,
+            builder: (context) {
+              return FractionallySizedBox(
+                heightFactor: 0.8,
+                child: Container(
+                  padding: EdgeInsets.all(8.0),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(12),
+                          topRight: Radius.circular(12))),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Flexible(
+                        child: Text(
+                          data.name,
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            color: Color(0xFF24366E),
+                            fontSize: Theme.of(context)
+                                .textTheme
+                                .displaySmall!
+                                .fontSize,
+                            fontWeight: FontWeight.w800,
+                          ),
+                        ),
+                      ),
+                      Flexible(
+                        child: Text(
+                          data.description,
+                          maxLines: 3,
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            color: Color(0xFF24366E).withOpacity(0.6),
+                            fontWeight: FontWeight.w800,
+                            fontSize: Theme.of(context)
+                                .textTheme
+                                .titleLarge!
+                                .fontSize,
+                            fontStyle: FontStyle.italic,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Flexible(
+                        child: Container(
+                          height: MediaQuery.of(context).size.width * 0.13,
+                          child: ListView.builder(
+                            scrollDirection: Axis.horizontal,
+                            itemBuilder: (context, index) {
+                              return InkWell(
+                                onTap: () {
+                                  Navigator.of(context).push(MaterialPageRoute(
+                                      builder: (context) =>
+                                          ImageFullView(data.images[index])));
+                                },
+                                child: AspectRatio(
+                                  aspectRatio: 1,
+                                  child: Hero(
+                                    tag: data.images[index],
+                                    child: Container(
+                                      width: MediaQuery.of(context).size.width *
+                                          0.1,
+                                      decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(8),
+                                          image: DecorationImage(
+                                            image: CachedNetworkImageProvider(
+                                              data.images[index],
+                                            ),
+                                            fit: BoxFit.fill,
+                                          )),
+                                    ),
+                                  ),
+                                ),
+                              );
+                            },
+                            itemCount: data.images.length,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
-              ),
-            ),
-            Expanded(
+              );
+            });
+      },
+      child: AspectRatio(
+        aspectRatio: 1.1,
+        child: Container(
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16),
+              color: Color(0xFF24366E),
+              image: DecorationImage(
+                  opacity: 0.4,
+                  fit: BoxFit.fill,
+                  image: data.backdrop.isEmpty
+                      ? const AssetImage(
+                          'assets/nyit_bear.png',
+                        )
+                      : CachedNetworkImageProvider(
+                          data.backdrop,
+                        ) as ImageProvider)),
+          margin: EdgeInsets.symmetric(vertical: 10),
+          width: MediaQuery.of(context).size.width * 0.85,
+          padding: EdgeInsets.symmetric(
+              horizontal: MediaQuery.of(context).size.width * 0.04,
+              vertical: 10),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Flexible(
                 child: Text(
-              data.description,
-              maxLines: 3,
-              style: TextStyle(
-                color: Colors.white70,
-                fontWeight: FontWeight.w800,
-                fontSize: Theme.of(context).textTheme.titleMedium!.fontSize,
-                fontStyle: FontStyle.italic,
-              ),
-            )),
-            // if (data.images.isNotEmpty)
-            //   ListView.builder(
-            //     itemBuilder: (context, index) {
-            //       if (index == data.images.length) {
-            //         return Icon(
-            //           Icons.keyboard_arrow_right,
-            //           size: 45,
-            //         );
-            //       }
-            //       return Container(
-            //         width: 60,
-            //         height: 60,
-            //         margin: EdgeInsets.all(10),
-            //         decoration: BoxDecoration(
-            //           borderRadius: BorderRadius.circular(16),
-            //           color: Colors.pinkAccent,
-            //         ),
-            //       );
-            //     },
-            //     itemCount: data.images.length + 1,
-            //   ),
-
-            if (data.actionBtnDisplay)
-              TextButton(
-                onPressed: () {},
-                child: Text(
-                  data.actionBtnText,
+                  data.name,
+                  textAlign: TextAlign.left,
                   style: TextStyle(
-                      fontSize:
-                          Theme.of(context).textTheme.headlineSmall!.fontSize),
+                    color: Colors.white,
+                    fontSize:
+                        Theme.of(context).textTheme.displaySmall!.fontSize,
+                    fontWeight: FontWeight.w800,
+                  ),
                 ),
-                style: TextButton.styleFrom(
-                    backgroundColor: Colors.yellow,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(16))),
               ),
-          ],
+              SizedBox(
+                height: 10,
+              ),
+              Flexible(
+                child: Text(
+                  data.description,
+                  maxLines: 3,
+                  style: TextStyle(
+                    color: Colors.white70,
+                    fontWeight: FontWeight.w800,
+                    fontSize: Theme.of(context).textTheme.titleMedium!.fontSize,
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ImageFullView extends StatelessWidget {
+  final String _imageUrl;
+  const ImageFullView(this._imageUrl, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        automaticallyImplyLeading: true
+      ),
+      backgroundColor: Colors.black,
+      body: InteractiveViewer(
+        child: Hero(
+          tag: _imageUrl,
+          child: Container(
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    fit: BoxFit.fill,
+                    image: CachedNetworkImageProvider(
+                      _imageUrl,
+                    ))),
+          ),
         ),
       ),
     );
